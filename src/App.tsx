20de3ac1502d494c9d342bf21088cce6808ed6e3
citrus <chengjunchao@hotmail.com>
2022-02-23 18:08:51 +0800

    ceshi

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Routes, Route, Link } from "react-router-dom";
-import Home from './Page/Home';
-import Hoc from './Page/Hoc';
-import Redux from './Page/Redux';
-import Hooks from './Page/Hooks';
-import Component from './Page/Component';
 import './App.css';
 
 
@@ -13,14 +8,7 @@ function App() {
   return (
     <div className="App">
       Hello， React
-      <Routes>
-        <Route path='/' element={<Home />}>
-          <Route path="component" element={<Component /> }></Route>
-          <Route path="hooks" element={<Hooks /> }></Route>
-          <Route path="hoc" element={<Hoc />}></Route>
-          <Route path="redux" element={<Redux />}></Route>
-        </Route>
-      </Routes>  
+
     </div>
   );
 }
